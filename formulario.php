50ebe4c6e719afcb79a2d30d0bcc43a133509346
Lucas Costa <dev.lucasgcosta@gmail.com>
2025-06-15 16:16:53 -0300

    Botão -voltar- foi estilizado

--- formulario.php
+++ formulario.php
@@ -41,6 +41,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Formulário | LG</title>
 
     <style>
@@ -118,7 +119,11 @@
     </style>
 </head>
 <body>
-    <a href="home.php">Voltar</a>
+    <a class="btn btn-md btn-primary ms-2" href="home.php" role="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"/>
+        </svg>
+    </a>
     <div class="box">
         <form action="formulario.php" method="POST">
             <fieldset>
